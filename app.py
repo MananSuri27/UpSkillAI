d5b675eef8341f9f5e7c7965a0d19e09acfb174a
Manan Suri <manansuri27@gmail.com>
2022-02-27 18:24:49 +0530

    added new route

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 import joblib
+import pandas as pd
 from flask_cors import CORS
+from bs4 import BeautifulSoup
+import requests
 
 
 
@@ -8,13 +11,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+dataset = pd.read_csv('Industry Skills.csv') 
+udemy = pd.read_csv('udemy.csv')
 
 classifier = joblib.load('./model/pipeline.pkl')
-# .\venv\Scripts\activate
 
-# make predict route a post route
-# add route to fetch newsmedia ranking
-# add route to get related news articles
 
 @app.route("/<name>")
 def home(name):
@@ -32,8 +33,41 @@ def predict():
 
     return jsonify({'pred': int(pred[0])})
 
+
            
-        
+@app.route('/show', methods =['POST'])
+def show():
+    json_=request.get_json()
+    industries = []
+    for x in json_['list']:
+        industry = list(set(dataset.loc[(dataset['skill_group_rank']==1) & (dataset['skill_group_name']==x)]['industry_name'].values))
+        for ind in industry:
+            i = ind
+            ind= ind.split()[0]
+            print(udemy.head())
+            course =  udemy.loc[udemy['title'].str.lower().str.contains(ind.lower())].values
+            print(course)
+            if len(course)==0:
+                link = "https://www.udemy.com/course/insights-into-integrity-ethics-and-morality-for-leaders/"
+                title = "Leadership Ethics and Integrity: A Comprehensive Guide !"
+            else:
+                link  = "https://www.udemy.com/course" + course[0][2]
+                title = course[0][1]
+
+            
+
+            
+            industries.append({
+                "industry":i,
+                "title": title,
+                "url": link
+            })
+    
+    print(industries)      
+    return jsonify({'industries': industries})
+
+
+
 
     
 
